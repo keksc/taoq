c5b686836efd3688bde8436c2b88376296f87562
keksc <pboursin2008@orange.fr>
2025-06-24 17:15:44 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,34 @@
 #include <random>
 #include <string>
 
+std::string getExecutableDir() {
+  std::ifstream exe("/proc/self/exe");
+  if (!exe.is_open()) {
+    return "";
+  }
+  // /proc/self/exe is a symlink, so read its target
+  char buffer[1024];
+  exe.read(buffer, sizeof(buffer) - 1);
+  std::streamsize len = exe.gcount();
+  if (len <= 0) {
+    return "";
+  }
+  buffer[len] = '\0';
+  // Find the last '/' to get the directory
+  std::string path(buffer);
+  std::size_t last_slash = path.find_last_of('/');
+  if (last_slash == std::string::npos) {
+    return "";
+  }
+  return path.substr(0, last_slash);
+}
 std::string pickRandomQuote() {
   static std::mt19937 rng(std::random_device{}());
   static std::uniform_int_distribution<std::streamsize> distrib;
   using paramType = decltype(distrib)::param_type;
 
   std::string quote;
-  if (std::ifstream file{"quotes.txt"}) {
+  if (std::ifstream file{getExecutableDir() + "quotes.txt"}) {
     std::streamsize linesRead = 0;
     std::string line;
 
@@ -25,13 +46,15 @@ std::string pickRandomQuote() {
         quote = line;
       }
     }
+  } else {
+    return "Couldnt open quotes.txt";
   }
   return quote;
 }
 
 int main() {
 
-  fmt::print("{}\n", pickRandomQuote());
+  fmt::print("{}", pickRandomQuote());
 
   return EXIT_SUCCESS;
 }
